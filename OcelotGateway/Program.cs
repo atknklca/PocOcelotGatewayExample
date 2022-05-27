80ca5e1afdf97518447480709826eb767eb4bf95
Atakan Kalça <atakan.kalca@kariyer.net>
2022-05-27 18:04:01 +0300

    initial commit

---
+++ OcelotGateway/Program.cs
@@ -0,0 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot(configuration);
+
+var app = builder.Build();
+
+app.UseOcelot();
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
